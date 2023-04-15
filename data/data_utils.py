541a8b67339ca9b8cf21ace5114b7152a2001474
gzy <709073685@qq.com>
2023-04-15 17:31:29 +0800

    add data process

---
+++ data/data_utils.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os
+
+
+def gen_image_set(path, des_path):
+    data_path = path + 'zte_data/'
+    for img_num in os.listdir(data_path):
+        max_pix = 0
+        max_name = ''
+        pic_path = data_path + img_num + '/'
+        for img_name in os.listdir(pic_path):
+            img = Image.open(pic_path + img_name)
+            pix = img.size[0] * img.size[1]
+            if (pix > max_pix):
+                max_pix = pix
+                max_name = pic_path + img_name
+        os.system('cp %s %s' % (max_name, des_path))
+
+
+
+gen_image_set('./','./new')
